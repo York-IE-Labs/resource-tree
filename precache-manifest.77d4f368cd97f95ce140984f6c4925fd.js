8364298dfb9ae219350d3f5f587bf67a113d775f
Nick Sjostrom <nick@sjostrom.io>
2020-10-28 14:24:19 -0400

    Updates

---
+++ precache-manifest.77d4f368cd97f95ce140984f6c4925fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecdd4f4149d97de75ba437c405049ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2dd4507ea5df8f3f2b4",
+    "url": "./static/css/main.d3b0b31b.chunk.css"
+  },
+  {
+    "revision": "657dc0c431b5709c4193",
+    "url": "./static/js/2.708fb2cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.708fb2cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2dd4507ea5df8f3f2b4",
+    "url": "./static/js/main.649d3503.chunk.js"
+  },
+  {
+    "revision": "a8a129e4b6bbc829e088",
+    "url": "./static/js/runtime-main.00910020.js"
+  }
+]);
